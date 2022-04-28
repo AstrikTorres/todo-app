fa17a7c15a460ba46c4d3ae53ca2dfa1b97fd287
AstrikTorres <astriktorres@gmail.com>
2022-04-28 13:51:02 -0500

    Se remplazaron los span por iconos de react y se agrego el llamado al modal para editar

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,25 +1,57 @@
 import React from "react";
 import "./TodoItem.css";
+import { TodoContext } from "../TodoContext";
+import { ImCheckboxUnchecked } from "react-icons/im";
+import { ImCheckboxChecked } from "react-icons/im";
+import { TiDelete } from "react-icons/ti";
+import { MdEdit } from "react-icons/md";
 
 function TodoItem(props) {
+  const { 
+    setOpenModalEdit,
+    setTodoValue,
+  } = React.useContext(TodoContext);
 
   return (
     <li
       className={ `todo-item ${ props.completed && 'todo-item--completed' }` }
     >
-      <span
-        className={`todo-check ${props.completed && 'check--checked'}`}
-        onClick={props.onComplete}
+      {
+        !props.completed && 
+        <ImCheckboxUnchecked
+          size="20px"
+          className={`todo-check`}
+          onClick={props.onComplete}
+        />
+      }
+      {
+        props.completed &&
+        <ImCheckboxChecked
+          size="20px"
+          className={`todo-check`}
+          onClick={props.onComplete}
+        />
+      }
+      <p 
+        className={`todo-text ${props.completed && 'todo-text--completed'}`}
       >
-        ᄼ
-      </span>
-      <p className={`todo-text ${props.completed && 'todo-text--completed'}`}>{props.text}</p>
-      <span
-        className="todo-delete"
-        onClick={props.onDelete}
-      >
-        X
-      </span>
+        {props.text}
+      </p>
+      <div className={"options-container"}>
+        <MdEdit
+          size="25px"
+          className="options-icon"
+          onClick={() => {
+            setTodoValue(props.text);
+            setOpenModalEdit(true);
+          }}
+        />
+        <TiDelete
+          size="30px"
+          className="options-icon options-icon--delete"
+          onClick={props.onDelete}
+        />
+      </div>
     </li>
   );
 }
